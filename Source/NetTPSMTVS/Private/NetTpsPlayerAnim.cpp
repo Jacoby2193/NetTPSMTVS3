d597eb4df817edf46de9938078627b45eef77a86
Jacoby2193 <jacobyi2193@gmail.com>
2024-08-26 13:59:44 +0900

    Initial commit

---
+++ Source/NetTPSMTVS/Private/NetTpsPlayerAnim.cpp
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NetTpsPlayerAnim.h"
+#include "NetTPSMTVSCharacter.h"
+
+void UNetTpsPlayerAnim::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	Me = Cast<ANetTPSMTVSCharacter>(TryGetPawnOwner());
+}
+
+void UNetTpsPlayerAnim::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	
+	if ( nullptr == Me )
+		return;
+	// 아래 두 값을 채우시오.
+	FVector vel = Me->GetVelocity();
+	FVector right = Me->GetActorRightVector();
+	FVector forward = Me->GetActorForwardVector();
+
+	Horizontal = FVector::DotProduct(vel , right);	// 좌우
+	
+	Vertical = FVector::DotProduct(vel , forward);	// 앞뒤
+
+	bHasPistol = Me->bHasPistol;
+}
---
+++ Source/NetTPSMTVS/Public/NetTpsPlayerAnim.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "NetTpsPlayerAnim.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NETTPSMTVS_API UNetTpsPlayerAnim : public UAnimInstance
+{
+	GENERATED_BODY()
+
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	UPROPERTY()
+	class ANetTPSMTVSCharacter* Me;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	bool bHasPistol;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	float Horizontal;	// Direction
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	float Vertical;		// Speed
+
+
+	
+};
