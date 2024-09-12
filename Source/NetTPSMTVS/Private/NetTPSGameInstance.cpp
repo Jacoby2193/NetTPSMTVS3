56ab5aaf6c55446d666ae731030d09bbee82afbd
Jacoby2193 <jacobyi2193@gmail.com>
2024-09-12 11:28:59 +0900

    20240912 1130
    Steamworks 환경 구성
     - Steamworks SDK 설치
     - OnlineSubsystem Steam 플러그인 설치
    Batch 파일로 언리얼 실행하기

---
+++ Source/NetTPSMTVS/Private/NetTPSGameInstance.cpp
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NetTPSGameInstance.h"
+#include "OnlineSubsystem.h"
+
+void UNetTPSGameInstance::Init()
+{
+	Super::Init();
+
+	if ( auto* subSystem = IOnlineSubsystem::Get() )
+	{
+		SessionInterface = subSystem->GetSessionInterface();
+	}
+
+}
---
+++ Source/NetTPSMTVS/Public/NetTPSGameInstance.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "NetTPSGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NETTPSMTVS_API UNetTPSGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Init() override;
+
+	// 온라인 세션 인터페이스를 기억하고싶다.
+	IOnlineSessionPtr SessionInterface;
+};
